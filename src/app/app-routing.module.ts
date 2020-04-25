7c052cca6e3c30fa643a78a48236da79889af280
ngeeannrobin <s10179209@connect.np.edu.sg>
2020-04-25 21:52:51 +0800

    reorganised menu files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { MainMenuComponent } from './main-menu/main-menu.component';
+// import { MainMenuComponent } from './menu/main-menu/main-menu.component';
+import { MenuComponent } from './menu/menu/menu.component';
 
 
 const routes: Routes = [
   {path: "login", component: LoginComponent},
-  {path: "mainmenu", component: MainMenuComponent},
+  {path: "menu", component: MenuComponent},
   {path: "", pathMatch: "full", redirectTo: "login"},
   {path: "**", redirectTo: "login"}
 ];
